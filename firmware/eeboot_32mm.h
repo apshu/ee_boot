aba1c97b972942e0705944a75c3518eadcd8ae15
apshu <34439880+apshu@users.noreply.github.com>
2020-10-02 16:30:58 +0200

    defined assert as SW BPT

--- firmware/eeboot_32mm.h
+++ firmware/eeboot_32mm.h
@@ -21,7 +21,12 @@ extern "C" {
 #define eeboot_weak_ram_func eeboot_ram_func __attribute__((weak))
 #define jump_to_app() do { __asm__ __volatile__("\tla $t0,%0\n" "\tjr $t0": : "" (eeboot_APP_START_ADDRESS) ); } while(0)
 #define self_reset() do { SYSKEY = 0; SYSKEY = 0xAA996655; SYSKEY = 0x556699AA; RSWRST = 1; unsigned long int bitBucket = RSWRST; } while(0)
-
+#if defined(NDEBUG) || !defined(__DEBUG)
+#define assert(X) do { ((void)0); } while(0)
+#else
+#define assert(X) do {((X) ? (void) 0 : __builtin_software_breakpoint()); } while(0)
+#endif
+    
 #ifdef	__cplusplus
 }
 #endif
