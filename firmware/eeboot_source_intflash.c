6695f72bd5e797a0500083c2aa9bf76386d0f1ad
apshu <34439880+apshu@users.noreply.github.com>
2020-10-05 13:30:03 +0200

    Fancy comments removed

--- firmware/eeboot_source_intflash.c
+++ firmware/eeboot_source_intflash.c
@@ -16,12 +16,10 @@ eeboot_ram_func bool EEBOOT_SOURCE_INTFLASH_NAMESPACE(seekReadData)(uint32_t off
 eeboot_ram_func uint32_t EEBOOT_SOURCE_INTFLASH_NAMESPACE(getCRCseekRead)(uint32_t crc32StartValue, uint32_t crc32Poly, uint32_t offset, size_t numBytes) {
     int k;
     uint8_t *buf = (uint8_t*)offset;
-//    crc32StartValue = ~crc32StartValue;
     while (numBytes--) {
         crc32StartValue ^= *buf++;
         for (k = 0; k < 8; k++)
             crc32StartValue = (crc32StartValue & 1) ? ((crc32StartValue >> 1) ^ crc32Poly) : (crc32StartValue >> 1);
     }
     return crc32StartValue;
-//    return ~crc32StartValue;
 }
